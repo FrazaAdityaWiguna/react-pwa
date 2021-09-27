e7f34c84a626466ca9a9b6afc41a499f759d95ab
FrazaAdityaWiguna <bigdreamnums@gmail.com>
2021-09-27 11:12:17 +0700

    feat: membuat page menjadi offline (manual add file)

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,32 @@
-console.warn('sw file in public folder');
+// menambahkan file yang akan di offline kan
+let cacheData = 'reactPWAv1';
+this.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheData).then((cache) => {
+      cache.addAll([
+        '/static/js/bundle.js',
+        '/static/js/main.chunk.js',
+        '/static/js/1.chunk.js',
+        '/static/js/vendors~main.chunk.js',
+        '/favicon.ico',
+        '/manifest.json',
+        '/logo192.png',
+        '/index.html',
+        '/about',
+        '/users',
+        '/',
+      ]);
+    })
+  );
+});
+
+// menjalankan fungsi sw
+this.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      if (resp) {
+        return resp;
+      }
+    })
+  );
+});
